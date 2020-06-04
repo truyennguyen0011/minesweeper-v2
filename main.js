66cce881ec3a134301906640ab17d75b94e6f970
Truyen <nguyentruyen28101999@gmail.com>
2020-06-05 00:13:44 +0700

    add minesweeper version 2

---
+++ main.js
@@ -0,0 +1,86 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+
+let win;
+
+function createWindow() {
+    // Create the browser window.
+    win = new BrowserWindow({
+        icon: __dirname + './src/img/icon.ico',
+        width: 1080,
+        height: 720,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    // and load the index.html of the app.
+    win.loadFile('index.html');
+
+    // Open the DevTools.
+    win.webContents.openDevTools();
+
+    // Remove menu
+    win.removeMenu();
+
+    // No resizing
+    win.setResizable(false);
+}
+
+// Create board from value received
+function setBoard(value) {
+    if (value === 'Easy') {
+        return {
+            rows: 8,
+            cols: 8,
+            rowWidth: 400,
+            rowHeight: 50,
+            mineNumbers: 10,
+        }
+    } else if (value === 'Medium') {
+        return {
+            rows: 14,
+            cols: 18,
+            rowWidth: 40 * 18,
+            rowHeight: 40,
+            mineNumbers: 40,
+        }
+    } else {
+        return {
+            rows: 20,
+            cols: 24,
+            rowWidth: 30 * 24,
+            rowHeight: 30,
+            mineNumbers: 99,
+        }
+    }
+}
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
+
+ipcMain.on('start:game', function (event, value) {
+    var prop = setBoard(value);
+    win.webContents.send('start:game', prop);
+});
+
---
+++ src/js/game.js
@@ -0,0 +1,237 @@
+ipcRenderer.on('start:game', function(e, prop) {
+    // startScreen.style.visibility = 'hidden';
+    startScreen.style.display = 'none';
+    var g = new Game(prop);
+});
+
+class Game {
+    constructor(prop) {
+        this.rows = prop.rows;
+        this.cols = prop.cols;
+        this.rowWidth = prop.rowWidth;
+        this.rowHeight = prop.rowHeight;
+        this.mineNumbers = prop.mineNumbers;
+        this.str = '';
+
+        // Create a board with the selected difficulty level
+        this.createBoard(this.rows, this.cols, this.rowWidth, this.rowHeight);
+
+        // Random positions of mines
+        this.randomPosMine(this.mineNumbers, this.rows, this.cols);
+
+        this.clickEvent = this.clickEvent.bind(this);
+        this.tryAgain = this.tryAgain.bind(this);
+
+        this.board.addEventListener('contextmenu', this.contextMenuEvent);
+
+        this.board.addEventListener('click', this.clickEvent);
+    }
+
+    contextMenuEvent(e) {
+        var tiles = e.target;
+        if (tiles.className === 'col') return;
+        if (tiles.classList.contains('flag')) {
+            tiles.classList.remove('flag');
+            tiles.classList.add('hidden');
+            tiles.src = './src/img/hidden.jpg';
+        } else {
+            tiles.classList.remove('hidden');
+            tiles.classList.add('flag');
+            tiles.src = './src/img/flag.jpg';
+        }
+    }
+
+    clickEvent(e) {
+        var tiles = e.target;
+        if (tiles.classList.contains('flag')) return;
+        if (tiles.className === 'col') return;
+        var row = parseInt(tiles.dataset.row);
+        var col = parseInt(tiles.dataset.col);
+
+        if (tiles.classList.contains("mine")) {
+            this.str = 'You Lose!';
+            this.gameOver();
+            switch (this.mineNumbers) {
+                case 10:
+                    setTimeout(this.tryAgain, 3000);
+                    break;
+                case 40:
+                    setTimeout(this.tryAgain, 12000);
+                    break;
+                default:
+                    setTimeout(this.tryAgain, 20000);
+                    break;
+            }
+        } else {
+            this.reveal(row, col);
+        }
+    }
+
+    createBoard(rows, cols, rowWidth, rowHeight) {
+        this.board = document.createElement('div');
+        this.board.setAttribute('class', 'board');
+
+        var header = document.createElement('div');
+        header.setAttribute('class', 'header');
+        
+        var content = document.createElement('div');
+        content.setAttribute('class', 'content');
+
+        for (let i = 0; i < rows; i++) {
+            var row = document.createElement('div');
+            row.setAttribute('class', 'row');
+            row.style.width = rowWidth + 'px';
+            row.style.height = rowHeight + 'px';
+            for (let j = 0; j < cols; j++) {
+                var col = document.createElement('img');
+                col.setAttribute('class', 'col hidden');
+                col.setAttribute('draggable', 'false');
+                col.setAttribute('data-row', i);
+                col.setAttribute('data-col', j);
+                col.src = './src/img/hidden.jpg';
+                col.style.width = rowWidth / cols + 'px';
+                col.style.height = rowHeight + 'px';
+
+                row.appendChild(col);
+            }
+            content.appendChild(row);
+        }
+        this.board.appendChild(header);
+        this.board.appendChild(content);
+        document.body.appendChild(this.board);
+    }
+
+    randomPosMine(numbers, rows, cols) {
+        var x, y;
+        var tiles;
+        for (let i = 0; i < numbers; i++) {
+            x = Math.floor(Math.random() * rows);
+            y = Math.floor(Math.random() * cols);
+            tiles = document.querySelectorAll(`[data-row="${x}"][data-col="${y}"]`);
+            if (!tiles[0].classList.contains('mine')) {
+                tiles[0].classList.add('mine');
+            } else {
+                i--;
+            }
+        }
+    }
+
+    reveal(row, col) {
+        this.helper(row, col);
+    }
+
+    helper(i, j) {
+        var colCount = this.countColOpen();
+        if (colCount === ((this.rows * this.cols) - this.mineNumbers)) {
+            this.gameWin();
+        }
+
+        if (i >= this.rows || j >= this.cols || i < 0 || j < 0) return;
+
+        var tiles = document.querySelectorAll(`[data-row="${i}"][data-col="${j}"]`);
+        var mineCount = this.getMineCount(i, j);
+
+        if (!tiles[0].classList.contains("hidden") || tiles[0].classList.contains("mine")) return;
+
+        tiles[0].classList.remove("hidden");
+
+        if (mineCount > 0) {
+            tiles[0].src = `./src/img/${mineCount}.jpg`;
+            return;
+        }
+
+        tiles[0].src = './src/img/blank.png';
+
+        for (let di = -1; di <= 1; di++) {
+            for (let dj = -1; dj <= 1; dj++) {
+                this.helper(i + di, j + dj);
+            }
+        }
+    }
+
+    getMineCount(i, j) {
+        let count = 0;
+
+        for (let di = -1; di <= 1; di++) {
+            for (let dj = -1; dj <= 1; dj++) {
+                var ni = i + di;
+                var nj = j + dj;
+                if (ni >= this.rows || nj >= this.cols || nj < 0 || ni < 0) {
+                    continue;
+                } else {
+                    var tiles = document.querySelectorAll(`[data-row="${ni}"][data-col="${nj}"]`);
+                    if (tiles[0].classList.contains("mine")) {
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+    
+    countColOpen() {
+        var count = 1;
+        var cols = document.querySelectorAll('.col');
+
+        cols.forEach(col => {
+            if (col.className === 'col') {
+                count++;
+            }
+        });
+        return count;
+    }
+
+    gameWin() {
+        this.str = 'You Win!';
+        this.board.removeEventListener('contextmenu', this.contextMenuEvent);
+        this.board.removeEventListener('click', this.clickEvent);
+
+        this.tryAgain();
+    }
+
+    gameOver() {
+        this.board.removeEventListener('contextmenu', this.contextMenuEvent);
+        this.board.removeEventListener('click', this.clickEvent);
+
+        var tiles = document.querySelectorAll('.mine');
+        var tilesLength = tiles.length;
+        var i = 0;
+
+        var id = setInterval(appearGradually, 200);
+
+        function appearGradually() {
+            if (i >= tilesLength - 1) {
+                clearInterval(id);
+                id = 0;
+            }
+            tiles[i].classList.remove('hidden');
+            tiles[i].src = './src/img/bomb.jpg';
+            i++;
+        }
+    }
+
+    tryAgain() {
+        var playAgain = document.createElement('div');
+        playAgain.setAttribute('class', 'play-again');
+
+        var againBtn = document.createElement('a');
+        var notification = document.createElement('label');
+
+        againBtn.setAttribute('class', 'again-btn');
+        againBtn.innerText = 'Try Again';
+
+        notification.setAttribute('class', 'text-lb');
+        notification.innerHTML = this.str;
+
+        playAgain.appendChild(notification);
+        playAgain.appendChild(againBtn);
+        document.body.appendChild(playAgain);
+
+        againBtn.addEventListener('click', () => {
+            this.board.remove();
+            playAgain.remove();
+            // startScreen.style.visibility = 'visible';
+            startScreen.style.display = 'flex';
+        });
+    }
+}
